5b6f3f0fca28bd83b5cf586827c9a1cc248c7b43
Jeffrey Charles <jeffreycharles@gmail.com>
2016-12-26 22:50:46 -0500

    Vertically align scores

--- src/components/ScoreBox.tsx
+++ src/components/ScoreBox.tsx
@@ -12,7 +12,7 @@ export default class extends React.Component<ScoreBoxProps, {}> {
   render() {
     return (
       <div style={{display: 'flex', minWidth: 85}}>
-        <div style={{border: border, boxSizing: 'border-box', textAlign: 'center', whiteSpace: 'pre-wrap', width: '50%'}}>{this.props.score !== null ? this.props.score : ' '}</div>
+        <div style={{alignItems: 'center', border: border, boxSizing: 'border-box', display: 'flex', justifyContent: 'center', whiteSpace: 'pre-wrap', width: '50%'}}>{this.props.score !== null ? this.props.score : ' '}</div>
         <div style={{boxSizing: 'border-box', width: '50%'}}>
           <div style={{border: border, textAlign: 'center', whiteSpace: 'pre-wrap'}}>{this.props.bid !== undefined ? this.props.bid : ' '}</div>
           <div style={{border: border, textAlign: 'center', whiteSpace: 'pre-wrap'}}>{this.props.tricks !== undefined ? this.props.tricks : ' '}</div>
