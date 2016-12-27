f2221b9875e860d886e4526c1daaa87936b5d102
Jeffrey Charles <jeffreycharles@gmail.com>
2016-12-26 22:44:17 -0500

    Dirty hack to get score column to render correctly

--- src/components/ScoreBox.tsx
+++ src/components/ScoreBox.tsx
@@ -11,7 +11,7 @@ const border = '1px solid black';
 export default class extends React.Component<ScoreBoxProps, {}> {
   render() {
     return (
-      <div style={{display: 'flex'}}>
+      <div style={{display: 'flex', minWidth: 85}}>
         <div style={{border: border, boxSizing: 'border-box', textAlign: 'center', whiteSpace: 'pre-wrap', width: '50%'}}>{this.props.score !== null ? this.props.score : ' '}</div>
         <div style={{boxSizing: 'border-box', width: '50%'}}>
           <div style={{border: border, textAlign: 'center', whiteSpace: 'pre-wrap'}}>{this.props.bid !== undefined ? this.props.bid : ' '}</div>
