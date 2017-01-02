2c81d485ee5c468d288518fa5ffd4e26b9a7a2d4
Jeffrey Charles <jeffreycharles@gmail.com>
2017-01-02 18:03:58 -0500

    Add new game link

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Content, Header, Layout} from 'react-mdl';
+import {Content, Header, Layout, Navigation} from 'react-mdl';
 
 import Game from './Game';
 import {PlayerInputs} from '../types';
@@ -10,14 +10,18 @@ export interface AppState {
   readonly inputs: {bids: PlayerInputs, tricks: PlayerInputs}[]
 };
 
+function getInitialState(): AppState {
+  const possiblePlayersString = sessionStorage.getItem('players');
+  const possibleInputsString = sessionStorage.getItem('inputs');
+  const players = (possiblePlayersString !== null && JSON.parse(possiblePlayersString)) || null;
+  const inputs = (possibleInputsString !== null && JSON.parse(possibleInputsString)) || [{bids: {}, tricks: {}}];
+  return {players, inputs: inputs};
+}
+
 export default class extends React.Component<{}, AppState> {
   constructor() {
     super();
-    const possiblePlayersString = sessionStorage.getItem('players');
-    const possibleInputsString = sessionStorage.getItem('inputs');
-    const players = (possiblePlayersString !== null && JSON.parse(possiblePlayersString)) || null;
-    const inputs = (possibleInputsString !== null && JSON.parse(possibleInputsString)) || [{bids: {}, tricks: {}}];
-    this.state = {players, inputs: inputs};
+    this.state = getInitialState();
   }
 
   onBeginGame(players: string[]) {
@@ -35,6 +39,12 @@ export default class extends React.Component<{}, AppState> {
     });
   }
 
+  onNewGame(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    sessionStorage.clear();
+    this.setState(getInitialState());
+  }
+
   onTricksSubmitted(round: number, tricks: PlayerInputs) {
     this.setState(prevState => {
       prevState.inputs[round] = prevState.inputs[round] || {bids: {}, tricks: {}};
@@ -46,9 +56,17 @@ export default class extends React.Component<{}, AppState> {
   }
 
   render() {
+    let newGame;
+    if (this.state.players) {
+      newGame = <a href="" onClick={e => this.onNewGame(e)}>New Game</a>;
+    }
     return (
       <Layout fixedHeader={true}>
-        <Header title="Wizard Scorekeeper" />
+        <Header title="Wizard Scorekeeper">
+          <Navigation>
+            {newGame}
+          </Navigation>
+        </Header>
         <Content>
           <div style={{paddingBottom: 30, paddingLeft: 30, paddingRight: 30}}>
             {this.state.players ?
